ebeecaae44c2963eb06896b4ecf0ca6bcf05ef4f
MazharAliFacctum <syed.ali@facctum.com>
2024-02-17 11:18:58 +0530

    Commmmm

--- deploy1/src/App.js
+++ deploy1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Mazhar Ali
+          Learn React With Syed Mazhar Ali
         </a>
       </header>
     </div>
